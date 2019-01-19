451f77ede8efbc538f7116009d8427ba9eff5b1c
jennifer kerssen <jjkerssen@gmail.com>
2019-01-19 10:50:52 -0700

    add BST algorithms

--- data-structures/binary-search-trees.js
+++ data-structures/binary-search-trees.js
@@ -1,6 +1,6 @@
 // BIG O RESULTS
 // Average case is O(log(n)) -- width and height grow logarithmically
-// Best case is O(1) if you need the root
+// Best case is O(1) if you need the root, worst is O(n) if you need the lowest branch
 
 
 // Initialize an empty BST 
@@ -66,26 +66,33 @@ class BinarySearchTree {
 
     // REMOVAL
     remove(key) {
+        // If this is the key you're trying to remove, 
         if (this.key == key) {
+            // If there is both a left and right child, 
+            // the right child with the lowest key replaces the removed node
             if (this.left && this.right) {
                 const successor = this.right._findMin();
                 this.key = successor.key;
                 this.value = successor.value;
                 successor.remove(successor.key);
             }
+            // If there is only one child node, it replaces the removed node
             else if (this.left) {
                 this._replaceWith(this.left);
             }
             else if (this.right) {
                 this._replaceWith(this.right);
             }
+            // If there is no child node, just remove the node
             else {
                 this._replaceWith(null);
             }
         }
+        // If your key is less that the one you're on, run the get method again from the left node
         else if (key < this.key && this.left) {
             this.left.remove(key);
         }
+        // If your key is greater than the one you're on, run the get method again from the right node
         else if (key > this.key && this.right) {
             this.right.remove(key);
         }
@@ -93,4 +100,106 @@ class BinarySearchTree {
             throw new Error('Key Error');
         }
     }
+    // REPLACING A REMOVED NODE
+    _replaceWith(node) {
+        // If the replacement node had a parent, update the parent references
+        if (this.parent) {
+            // Update reference FROM parent
+            if (this == this.parent.left) {
+                this.parent.left = node;
+            }
+            else if (this == this.parent.right) {
+                this.parent.right = node;
+            }
+            // Update reference TO parent
+            if (node) {
+                node.parent = this.parent;
+            }
+        }
+         
+        else {
+            // If the removed node was the root, copy over all refs from the replacement
+            if (node) {
+                this.key = node.key;
+                this.value = node.value;
+                this.left = node.left;
+                this.right = node.right;
+            }
+            // If the removed node was the only node, empty the tree
+            else {
+                this.key = null;
+                this.value = null;
+                this.left = null;
+                this.right = null;
+            }
+        }
+    }
+    // FINDING THE REPLACEMENT NODE
+    // Keep looking left until you get to the end of the branch
+    _findMin() {
+        if (!this.left) {
+            return this;
+        }
+        return this.left._findMin();
+    }
 }
+
+
+// ALGORITHM PRACTICE
+
+let tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+
+// Write an algorithm to find the height of a binary search tree
+// Height here counts the branches, not the root
+function treeHeight(tree){
+    if(!tree) return -1;
+    
+    let leftHeight = treeHeight(tree.left);
+    let rightHeight = treeHeight(tree.right);
+
+    return Math.max(leftHeight, rightHeight) + 1;
+
+    // With if instead of Math method:
+    /*
+    if (leftHeight > rightHeight) {
+        return leftHeight + 1;
+    } else {
+        return rightHeight + 1;
+    }
+    */
+};
+
+treeHeight(tree);
+
+// Write an algorithm to check whether an arbitrary binary tree is a binary search tree, 
+// assuming the tree does not contain duplicates
+function checkBST(node, min = null, max = null) {
+    if (max !== null && node.data > max) {
+      return false;
+    }
+  
+    if (min !== null && node.data < min) {
+      return false;
+    }
+  
+    if (node.left && !checkBST(node.left, min, node.data)) {
+      return false;
+    }
+  
+    if (node.right && !checkBST(node.right, node.data, max)) {
+      return false;
+    }
+  
+    return true;
+};
+
+checkBST(tree);
+
+//Write an algorithm to find the third largest node in a binary search tree

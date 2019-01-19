b68a615c720649bd8534f731ff44303bfcb4ba54
jennifer kerssen <jjkerssen@gmail.com>
2019-01-18 21:54:58 -0700

    add hash map implementation

---
+++ data-structures/hash-maps.js
@@ -0,0 +1,95 @@
+// HASH MAP IMPLEMENTATION
+
+// The _slots array will hold all of the data
+class HashMap {
+    constructor(initialCapacity=8) {
+      this.length = 0;
+      this._slots = [];
+      this._capacity = initialCapacity;
+    }
+    // Uses djb2 algorithm to hash a string and output a number
+    static _hashString(string) {
+      let hash = 5381;
+      for (let i=0; i<string.length; i++) {
+        hash = (hash << 5) + hash + string.charCodeAt(i);
+        hash = hash & hash;
+      }
+      return hash >>> 0;
+    }
+    
+    // Implementation with Separate Chaining
+    /*
+    set(key,value){
+        console.log(this._slots[key]);
+        if(!this._slots[key]){
+        this._slots[key] = [{key, value}];
+        }
+        this._slots[key].push({key, value});
+    }
+    */
+    
+    // Implementation with Open Addressing
+    set(key, value) {
+      // Check the capacity and resize if necessary
+      // Ensures that the array will never fill up
+      const loadRatio = (this.length + 1) / this._capacity;
+      if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+        this._resize(this._capacity * HashMap.SIZE_RATIO);
+      }
+      // 
+      const index = this._findSlot(key);
+      this._slots[index] = {
+        key,
+        value
+      };
+      this.length++;
+    }
+    // Finds the slot by index, checks that it has an item
+    remove(key) {
+      const index = this._findSlot(key);
+      const slot = this._slots[index];
+      if (slot === undefined) {
+          throw new Error('Key error');
+      }
+      // Marks the slot as deleted, reduces the active length and increments the deleted length
+      slot.deleted = true;
+      this.length--;
+      this._deleted++;
+    }
+  
+    // Finds the correct slot given the hashed key
+    _findSlot(key) {
+      // Calculate the hash of the string
+      const hash = HashMap._hashString(key);
+      // Fit the object within the current capacity
+      const start = hash % this._capacity;
+      // Finds the matching key, or an empty slot if the matching slot is full
+      for (let i=start; i<start + this._capacity; i++) {
+        const index = i % this._capacity;
+        const slot = this._slots[index];
+        // Avoids slots containing a deleted item
+        if (slot === undefined || (slot.key == key && !slot.deleted)) {
+            return index;
+        }
+      }
+    }
+  
+    // Creates a new hash map with a larger capacity
+    _resize(size) {
+      const oldSlots = this._slots;
+      this._capacity = size;
+      // Reset map and deleted lengths. They will get rebuilt as you add the items back
+      this.length = 0;
+      this._deleted = 0;
+      this._slots = [];
+      // For each non-empty, non-deleted slot from the old map, place the items in the new map. 
+      for (const slot of oldSlots) {
+        if (slot !== undefined && !slot.deleted) {
+          this.set(slot.key, slot.value);
+        }
+      }
+    }
+  }
+  
+  HashMap.MAX_LOAD_RATIO = 0.9;
+  HashMap.SIZE_RATIO = 3;
